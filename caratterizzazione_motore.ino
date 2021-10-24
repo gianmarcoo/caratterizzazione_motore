382b84eba82b56986155630c1fc5937083f796b1
gianmarcoo <84909890+gianmarcoo@users.noreply.github.com>
2021-10-24 20:49:25 +0200

    Update caratterizzazione_motore.ino

--- caratterizzazione_motore.ino
+++ caratterizzazione_motore.ino
@@ -1,15 +1,15 @@
 // CONFIGURAZIONE PIN
 #define PIN_EN    0
-#define PIN_IN1   1
-#define PIN_IN2   2
-#define PIN_PWM   3
-#define PIN_ENC_A 4
-#define PIN_ENC_B 5 
+#define PIN_IN1   8
+#define PIN_IN2   9
+#define PIN_PWM   4
+#define PIN_ENC_A 2
+#define PIN_ENC_B 3 
 
 void setup() {
   // SERIALE
   Serial.begin(115200);
-  Serial.println("Format: (time in milliseconds,direction of the step)");
+  Serial.println("Format: (time in milliseconds, direction of the step)");
 
 
   // DRIVER MOTORI
@@ -33,16 +33,17 @@ void setup() {
   int tempo;
   for(int i = 0; i < 2; i++) {
     // avanti
-    pinMode(PIN_IN1,HIGH);
-    pinMode(PIN_IN2,LOW);
+    digitalWrite(PIN_IN1,HIGH);
+    digitalWrite(PIN_IN2,LOW);
+    analogWrite(PIN_PWM, 255);
 
     // aspetto un secondo
     tempo = millis();
     while(millis() < tempo + 1000);
 
     // indietro
-    pinMode(PIN_IN1,LOW);
-    pinMode(PIN_IN2,HIGH);
+    digitalWrite(PIN_IN1,LOW);
+    digitalWrite(PIN_IN2,HIGH);
 
     // aspetto un secondo
     tempo = millis();
